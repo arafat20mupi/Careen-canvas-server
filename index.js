25153f82381acf62c3f6e7a4e4c625de4f528e27
NahiyaNasir <nahiaesha.01@gmail.com>
2024-09-27 14:28:59 +0600

    Project setup done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+// Middleware
+app.use(express.json());
+//  Home route
+app.get("/", (req, res) => {
+  res.send("hello Developer");
+});
+// Server listening
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
